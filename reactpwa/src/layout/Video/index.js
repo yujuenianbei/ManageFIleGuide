5dd40f8529b87592d28fc7a36df25a80df303cd1
yujuenianbei <365913672@qq.com>
2019-03-29 00:17:57 +0800

    ver 0.5

--- reactpwa/src/layout/Video/index.js
+++ reactpwa/src/layout/Video/index.js
@@ -18,15 +18,32 @@ class Video extends Component {
             currentTime: '00:00',
             duration: '00:00',
             videLoop: false,
-            controllerShow: 'block'
+            controllerShow: 'block',
+            fullWidth: false,
+            showContoller: true,
+            url: ''
         }
     }
     componentWillMount = () => {
         this.props.getVideo(this.props.match.params.id);
     }
     componentDidMount = () => {
-        console.log(0)
         clearInterval(this.timer);
+        this.controller = setTimeout(
+            () => {
+                this.setState({ showContoller: false })
+            }, 3000
+        )
+    }
+    shouldComponentUpdate(nextProps) {
+        if (nextProps.page.Video.videoData !== this.props.page.Video.videoData || this.props.page.Video.videoData != '') {
+            return true
+        } else {
+            return false
+        }
+    }
+    componentWillReceiveProps = (nextProps)=> {
+        this.setState({url:this.props.page.Http.video + nextProps.page.Video.videoData.video_url})
     }
 
     // 时间转换
@@ -61,6 +78,14 @@ class Video extends Component {
             ele.webkitRequestFullScreen();
         }
         this.setState({ fullScreen: true });
+        this.fullpage = setInterval(
+            () => {
+                if (window.screen.width > window.screen.height) {
+                    this.setState({ fullWidth: true })
+                } else {
+                    this.setState({ fullWidth: false })
+                }
+            }, 1000)
     }
     //退出全屏
     exitFullscreen = () => {
@@ -72,21 +97,22 @@ class Video extends Component {
         } else if (de.webkitCancelFullScreen) {
             de.webkitCancelFullScreen();
         }
-        this.setState({ fullScreen: false });
+        this.setState({ fullScreen: false, fullWidth: false });
+        clearInterval(this.fullpage)
     }
-
+    // 暂停、播放
     videoPause = () => {
-        console.log(window.orientation)
+        // console.log(window.orientation)
         var video = document.getElementById('video');
-        console.log(video)
+        // console.log(video)
         if (this.state.pause === true) {
             video.play();
-            console.log(video.currentTime);
-            console.log(video.volume);
-            console.log(video.duration);
+            // console.log(video.currentTime);
+            // console.log(video.volume);
+            // console.log(video.duration);
             video.oncanplay = () => {
-                console.log(video.duration)
-                // this.setState({ videoFullTime: video.duration })
+                // console.log(video.duration)
+                this.setState({ videoFullTime: video.duration })
             }
             this.timer = setInterval(
                 () => {
@@ -103,68 +129,75 @@ class Video extends Component {
             this.setState({ pause: true })
         }
     }
+    // 
+    showController = () => {
+        clearTimeout(this.controller);
+        clearTimeout(this.nextController);
+        this.setState({ showContoller: true })
+        this.nextController = setTimeout(
+            () => {
+                this.setState({ showContoller: false })
+            }, 5000
+        )
+
+    }
     render() {
         return (
-            this.props.page.Video.videoData && <div className="video">
-                <div id='videoPlayer' className="video_Content" style={{ position: 'relative', width: '100%' }}>
-                    <video
-                        objectfit='contain'
-                        id='video'
-                        width='100%'
-                        height={this.state.fullScreen ? '100%' : 'auto'}
-                        loop={this.state.videLoop}
-                        controls={false}
-                        onDoubleClick={this.videoPause}
-                        poster={this.props.page.Http.img + this.props.page.Video.videoData.video_img}
-                        webkit-playsinline="true"
-                        playsInline={true}
-                        x-webkit-airplay="true"
-                        x5-video-player-type='h5'
-                        x5-video-player-fullscreen="true"
-                        x5-video-ignore-metadata="true"
-                    >
-                        <source src={this.props.page.Http.video + this.props.page.Video.videoData.video_url} type="video/mp4" />
-                        your browser does not support the video tag
+            this.props.page.Video.videoData && this.state.url && <div className="video">
+                <div id='videoPlayer' className="video_Content" style={{ position: 'relative', width: '100%', height: this.state.fullScreen ? '100%' : '13rem' }} onTouchMove={this.showController}>
+                    <div className='videoPlayer_Content' objectfit='contain' style={{ margin: '0 auto', width: this.state.fullWidth ? '100%' : 'auto', height: !this.state.fullWidth ? '100%' : 'auto' }}>
+                        <video
+                            id='video'
+                            width={!this.state.fullWidth ? '100%' : 'auto'}
+                            height={this.state.fullScreen && this.state.fullWidth ? window.screen.height : '100%'}
+                            loop={this.state.videLoop}
+                            controls={false}
+                            onClick={this.showController}
+                            onDoubleClick={this.videoPause}
+                            poster={this.props.page.Http.img + this.props.page.Video.videoData.video_img}
+                            webkit-playsinline="true"
+                            playsInline={true}
+                            x-webkit-airplay="true"
+                            x5-video-player-type='h5'
+                            x5-video-player-fullscreen="true"
+                            x5-video-ignore-metadata="true"
+                        >
+                            <source src={this.state.url} type="video/mp4" />
+                            your browser does not support the video tag
                     </video>
-                    <div id='controller' className={this.state.fullScreen ? 'videoController videoFullScreen' : 'videoController videoNotFullScreen'}
-                        style={{ 
-                            display: this.state.controllerShow, 
-                            top: this.state.fullScreen? 'auto': '-36px',
-                            bottom: this.state.fullScreen? '0': 'auto',
-                            position: this.state.fullScreen? 'absolute': 'relative',
-                        }}
-                        onMouseEnter={this.notFullScreenController} >
-                        <span className='videoButton videoButtonLeft playButton'>
-                            <Tooltip title={this.state.pause ? '播放' : '暂停'} placement="bottom">
+                        <div id='controller' className={this.state.fullScreen ? 'videoController videoFullScreen' : 'videoController videoNotFullScreen'}
+                            style={{
+                                // left: this.state.fullWidth?  (document.getElementById('videoPlayer').clientWidth - document.getElementById('video').clientWidth)/2' : '0',
+                                // width: this.state.fullWidth? document.getElementById('video').clientWidth: '100%', 
+                                width: '100%',
+                                top: this.state.fullScreen ? 'auto' : this.state.showContoller ? '-36px' : '-5px',
+                                bottom: this.state.fullScreen ? (this.state.showContoller ? '0' : '-36px') : 'auto',
+                                position: this.state.fullScreen ? 'absolute' : 'relative',
+                            }}
+                            onMouseEnter={this.notFullScreenController} >
+                            <span className='videoButton videoButtonLeft playButton'>
                                 <Icon type={this.state.pause ? "caret-right" : "pause"} onClick={this.videoPause} style={{ color: '#fff' }} />
-                            </Tooltip>
-                        </span>
-                        {/* 200px */}
-                        <div style={{ width: 'calc(100% - 155px)', float: 'left', marginLeft: 10 }}>
-                            <Slider tipFormatter={null} value={this.state.videoCurrentTimeSlider} onChange={this.videoLengthChange} onAfterChange={this.videoLengthChange} />
-                        </div>
-                        {/* <div style={{ width: 40, height: 30, float: "right", marginLeft: 10 }}>
-                            <Slider tipFormatter={null} value={this.state.volume} onChange={this.soundChange} onAfterChange={this.soundChange} />
-                        </div> */}
-                        <span className='videoButton videoButtonRight playButton'>
-                            <Tooltip title={this.state.fullScreen ? '关闭全屏' : '打开全屏'} placement="bottom">
+                            </span>
+                            {/* 200px */}
+                            <div style={{ width: 'calc(100% - 170px)', float: 'left', marginLeft: 10 }}>
+                                <Slider tipFormatter={null} value={this.state.videoCurrentTimeSlider} onChange={this.videoLengthChange} onAfterChange={this.videoLengthChange} />
+                            </div>
+                            <span className='videoButton videoButtonRight playButton'>
                                 <Icon type={this.state.fullScreen ? "fullscreen-exit" : "fullscreen"} style={{ color: '#fff' }} theme="outlined" onClick={this.state.fullScreen ? this.exitFullscreen : this.FullScreen} />
-                            </Tooltip>
-                        </span>
-                        <span className='videoButton videoButtonRight playButton'>
-                            <Tooltip title={this.state.videLoop ? '关闭无脑循环' : '打开无脑循环'} placement="bottom">
+
+                            </span>
+                            <span className='videoButton videoButtonRight playButton'>
                                 <Icon type={this.state.videLoop ? "retweet" : "swap"} theme="outlined" onClick={this.videoLoopState} style={{ color: '#fff' }} />
-                            </Tooltip>
-                        </span>
-                        <span className='timer videoButton videoButtonRight'>
-                            {this.state.currentTime}/{this.state.duration}
-                        </span>
-                    </div>
-                    {/* <div className='videoPause' style={{ display: this.state.showPlayBtn ? "block" : "none" }}>
+                            </span>
+                            <span className='timer videoButton videoButtonRight'>
+                                {this.state.currentTime}/{this.state.duration}
+                            </span>
+                        </div>
+                        {/* <div className='videoPause' style={{ display: this.state.showPlayBtn ? "block" : "none" }}>
                     <Icon className='videoPauseIcon' type={this.state.pause ? "play-circle" : "pause-circle"} onClick={this.videoPause} />
                 </div> */}
+                    </div>
                 </div>
-
             </div>
         )
     }
